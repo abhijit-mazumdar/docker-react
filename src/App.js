3eecc2f4f40edac2440d9d029960622a0cafd55a
Abhijit Mazumdar <abhijitmazumdar@Abhijits-MacBook-Pro.local>
2020-02-15 13:55:37 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cuckoo there from Abhijit!
         </p>
         <a
           className="App-link"
